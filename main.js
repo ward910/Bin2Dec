04f4ad743ea11547da938f59e383fa95df223b6f
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-06-28 14:24:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function calcular() {
-        const number = document.getElementById("Bin");
-        const dec = document.getElementById("Dec");
-        const n = parseInt(number.value, 2);
+        const number = document.getElementById("Bin"); // Id de Bin
+        const dec = document.getElementById("Dec"); // Id de Dec
+        const n = parseInt(number.value, 2); // Transforma um numero binario en decimal
         
         dec.value = n;
     }
